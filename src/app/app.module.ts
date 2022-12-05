ee2b765b3b48cd3ee11d0eeab7bae5c7c506ab0a
Francisco Jasso <pacojare_0223@hotmail.com>
2022-12-05 17:25:38 -0600

    fin seccion 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroesModule } from './heroes/heroes.module';
 import { ContadorModule } from './contador/contador.module';
+import { DbzModule } from './dbz/dbz.module';
 
 
 @NgModule({
@@ -13,7 +14,9 @@ import { ContadorModule } from './contador/contador.module';
   imports: [
     BrowserModule,
     HeroesModule, 
-    ContadorModule
+    ContadorModule,
+    DbzModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
